bbaa9476ee1d025e8855243e0cc0f3a6d8213e8f
yuichi-sugiyama <yuichi-sugiyama@cybozu.co.jp>
2019-11-02 15:49:59 +0900

    選択時にポップアップを出すように

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
 (function(){
     const appID = 45;
     const fieldID = "f5517797";
-
-    function main ()
-    {
-        if (!isTrigerKey(event)){
-            return;
-        }
-        openKintoneSearchScreen();
-    }
-
-    function isTrigerKey(event)
-    {
-        return event.keyCode === 13;
-    }
     
     function getSelectedText()
     {
@@ -35,5 +22,35 @@
         window.open(searchURL, '_blank');
     }
 
-    document.onkeydown = main;
+    function makePopup(event)
+    {
+        closePopup();
+        const selectedText = getSelectedText();
+        if (selectedText === ''){
+            return;
+        }
+        const popup = getPopupHTML(event.x, event.y + 60);      
+        document.body.appendChild(popup);
+    }
+
+    function getPopupHTML(x, y)
+    {
+        const popup = document.createElement("button");
+        popup.setAttribute("class", "kintone-search-popup");
+        popup.setAttribute("title", "活動履歴を検索する");
+        popup.onclick = openKintoneSearchScreen;
+        popup.style.top = y + "px";
+        popup.style.left = x + "px";
+        return popup;
+    }
+
+    function closePopup()
+    {
+        const targetElements = document.getElementsByClassName('kintone-search-popup');
+	    for (let i = 0; i < targetElements.length; i++) {
+            targetElements[i].parentNode.removeChild(targetElements[i]);
+        }
+  
+    }
+    document.getElementsByClassName('mainarea')[0].onmouseup = makePopup;
 }());
